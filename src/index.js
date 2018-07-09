914e6fce5ded20743fa9620527c532b24d7288e6
limengke <752140235@qq.com>
2018-07-09 13:20:02 +0800

    v2版本react

--- src/index.js
+++ src/index.js
@@ -1,76 +1,30 @@
-const React = {
-    createElement
-}
+import React from './react'
+import ReactDom from './react-dom'
 
-const ReactDom = {
-    render: (vnode, container) => {
-        container.innerHTML = ''
-        return render(vnode, container)
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            num: 1
+        }
     }
-}
-
-function createElement (tag, attrs, ...children) {
-    return {
-        tag,
-        attrs,
-        children
-    }
-}
-
-function render (vnode, container) {
-    if (typeof vnode === 'string') {
-        let textNode = document.createTextNode(vnode)
-        return container.appendChild(textNode)
-    }
-    const dom = document.createElement(vnode.tag)
-    if (vnode.attrs) {
-        Object.keys(vnode.attrs).forEach(key => {
-            const value = vnode.attrs[key]
-            setAttribute(dom, key, value)
+    onClick () {
+        this.setState({
+            num: this.state.num + 1
         })
     }
-    vnode.children.forEach(child => render(child, dom))
-    return container.appendChild(dom)
-}
-
-function  setAttribute (dom, name, value) {
-    if (name === 'className') {
-        name = 'class'
-    }
-    if (/on\w+/.test(name)) {
-        name = name.toLowerCase()
-        dom[name] = value || ''
-    } else if (name === 'style') {
-        if (!value || typeof value === 'string') {
-            dom.style.cssText = value || ''
-        } else if (value && typeof value === 'object') {
-            for (let styleName in value) {
-                dom.style[styleName] = typeof value[styleName] === 'number' ? value[styleName] + 'px' : value[styleName]
-            }
-        }
-    } else {
-        if (name in dom ) {
-            dom[name] = value || ''
-        }
-        if (value) {
-            dom.setAttribute(name, value)
-        } else {
-            dom.removeAttribute(name, value)
-        }
+    render () {
+        return (
+            <div>
+                <h1>count: {this.state.num}</h1>
+                <button onClick={() => this.onClick()}>add</button>
+            </div>
+        )
     }
 }
 
-function tick () {
-    const element = (
-        <div>
-            <h1>hello world</h1>
-            <h2>it is {new Date().toLocaleTimeString()}</h2>
-        </div>
-    )
-    ReactDom.render(
-        element,
-        document.getElementById('root')
-    )
-}
 
-setInterval(tick, 1000)
+ReactDom.render(
+    <Counter/>,
+    document.getElementById('root')
+)
