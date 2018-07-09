d1e2864a76b53df8fe9b2e0abf846122f40008ec
limengke <752140235@qq.com>
2018-07-09 11:19:31 +0800

    v1版本react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,76 @@
- import Mvvm from "./mvvm";
+const React = {
+    createElement
+}
 
-(function (global, mvvm) {
-    global.Mvvm = mvvm
-})(window, Mvvm)
+const ReactDom = {
+    render: (vnode, container) => {
+        container.innerHTML = ''
+        return render(vnode, container)
+    }
+}
+
+function createElement (tag, attrs, ...children) {
+    return {
+        tag,
+        attrs,
+        children
+    }
+}
+
+function render (vnode, container) {
+    if (typeof vnode === 'string') {
+        let textNode = document.createTextNode(vnode)
+        return container.appendChild(textNode)
+    }
+    const dom = document.createElement(vnode.tag)
+    if (vnode.attrs) {
+        Object.keys(vnode.attrs).forEach(key => {
+            const value = vnode.attrs[key]
+            setAttribute(dom, key, value)
+        })
+    }
+    vnode.children.forEach(child => render(child, dom))
+    return container.appendChild(dom)
+}
+
+function  setAttribute (dom, name, value) {
+    if (name === 'className') {
+        name = 'class'
+    }
+    if (/on\w+/.test(name)) {
+        name = name.toLowerCase()
+        dom[name] = value || ''
+    } else if (name === 'style') {
+        if (!value || typeof value === 'string') {
+            dom.style.cssText = value || ''
+        } else if (value && typeof value === 'object') {
+            for (let styleName in value) {
+                dom.style[styleName] = typeof value[styleName] === 'number' ? value[styleName] + 'px' : value[styleName]
+            }
+        }
+    } else {
+        if (name in dom ) {
+            dom[name] = value || ''
+        }
+        if (value) {
+            dom.setAttribute(name, value)
+        } else {
+            dom.removeAttribute(name, value)
+        }
+    }
+}
+
+function tick () {
+    const element = (
+        <div>
+            <h1>hello world</h1>
+            <h2>it is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    )
+    ReactDom.render(
+        element,
+        document.getElementById('root')
+    )
+}
+
+setInterval(tick, 1000)
--- test/observer.test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect
-
-describe('expect1111', () => {
-    it('4 + 5应该等于9', () => {
-        expect(4 + 5).to.be.equal(9)
-    })
-})
